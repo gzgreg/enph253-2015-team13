74494a469e5cd89be7b2d3ddbb138cdef03520b1
gzgreg <gzgregoryzhang@gmail.com>
2015-06-07 13:05:30 -0700

    add PID tape follower algorithm

---
+++ Robot/Code/tape_follower/tape_follower.ino
@@ -0,0 +1,42 @@
+#include <phys253.h>          
+#include <LiquidCrystal.h>    
+
+#define P_PIN 6
+#define I_PIN 7
+#define LEFT_SENSOR 0
+#define RIGHT_SENSOR 1
+#define LINE_THRESHOLD 30
+#define ERR_LIMIT 10
+
+int P, I, D, errInt, errDeriv, prevErr;
+
+void setup()
+{  
+  #include <phys253setup.txt>
+  Serial.begin(9600) ;
+  
+  errInt = 0;
+  prevErr = 0;
+  D = 0;
+}
+
+void loop()
+{
+  int leftSensor = analogRead(LEFT_SENSOR);
+  int rightSensor = analogRead(RIGHT_SENSOR);
+  int leftErr = (leftSensor > LINE_THRESHOLD) ? 1 : 0;
+  int rightErr = (rightSensor > LINE_THRESHOLD) ? 1 : 0;
+  
+  int totalErr = leftErr - rightErr;
+  if(leftErr + rightErr > 0 && totalErr == 0){
+    //off line: set error to be same sign as previous error
+    if(prevErr < 0) totalErr = -2;
+    if(prevErr > 0) totalErr = 2;
+  }
+  
+  errDeriv = totalErr - prevErr;
+  errInt = errInt + totalErr;
+  if(errInt > ERR_LIMIT) errInt = ERR_LIMIT;
+  if(errInt < -ERR_LIMIT) errInt = -ERR_LIMIT;  
+  int correct = P*totalErr + I*errInt + D*errDeriv;
+}

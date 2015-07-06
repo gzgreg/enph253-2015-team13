132714e3ee75db4e98bdc1f13edf0ee2ff280917
gzgreg <gzgregoryzhang@gmail.com>
2015-07-06 14:07:05 -0700

    add ir following code

---
+++ Robot/Code/robotIR/preprocessor.h
@@ -0,0 +1,80 @@
+//Include this file in all robot program folders.
+
+#include <phys253.h>
+#include <LiquidCrystal.h>
+
+#define LEFT_SENSOR 0
+#define RIGHT_SENSOR 1
+#define LEFT_IR 6
+#define RIGHT_IR 7
+#define LEFT_MOTOR 1
+#define RIGHT_MOTOR 0
+#define BUTTON_WAIT 200
+
+#include <avr/EEPROM.h>
+ 
+class MenuItem{
+public:
+  String    Name;
+  int Value;
+  uint16_t* EEPROMAddress;
+  int Max;
+  static uint16_t MenuItemCount;
+  MenuItem(String name, int max){
+    MenuItemCount++;
+    EEPROMAddress = (uint16_t*)(2 * MenuItemCount);
+    Name = name;
+    Value = (int) eeprom_read_word(EEPROMAddress);
+    Max = max;
+  }
+  void Save(){
+    eeprom_write_word(EEPROMAddress, Value);
+  }
+};
+ 
+uint16_t MenuItem::MenuItemCount = 0;
+/* Add the menu items here */
+MenuItem Speed = MenuItem("Speed", 255);
+MenuItem PTape = MenuItem("P-Tape", 1023);
+MenuItem DTape = MenuItem("D-Tape", 1023);
+MenuItem Thresh = MenuItem("Thresh-Tape", 1023);
+MenuItem PIR = MenuItem("P-IR", 1023);
+MenuItem DIR = MenuItem("D-IR", 1023);
+MenuItem menuItems[] = {Speed, PTape, DTape, Thresh, PIR, DIR};
+ 
+void Menu(){
+  LCD.clear(); LCD.home();
+  LCD.print("Entering menu");
+  delay(500);
+ 
+  while (true){
+    /* Show MenuItem value and knob value */
+    int menuIndex = knob(6) * (MenuItem::MenuItemCount) / 1024;
+    LCD.clear(); LCD.home();
+    LCD.print(menuItems[menuIndex].Name); LCD.print(" "); LCD.print(menuItems[menuIndex].Value);
+    LCD.setCursor(0, 1);
+    LCD.print("Set to "); LCD.print(map(knob(7), 0, 1024, 0, menuItems[menuIndex].Max + 1)); LCD.print("?");
+    delay(100);
+ 
+    /* Press start button to save the new value */
+    if (startbutton()){
+      delay(50);
+      if (startbutton()){
+        menuItems[menuIndex].Value = map(knob(7), 0, 1024, 0, menuItems[menuIndex].Max + 1);
+        menuItems[menuIndex].Save();
+        delay(250);
+      }
+    }
+ 
+    /* Press stop button to exit menu */
+    if (stopbutton()){
+      delay(50);
+      if (stopbutton()){
+        LCD.clear(); LCD.home();
+        LCD.print("Leaving menu");
+        delay(500);
+        return;
+      }
+    }
+  }
+}
---
+++ Robot/Code/robotIR/robotIR.ino
@@ -0,0 +1,81 @@
+#include "preprocessor.h"
+#include <phys253.h>
+#include <LiquidCrystal.h>
+
+int errDeriv, prevErr, errTime, prevErrTime, threshold, i;
+
+void setup()
+{
+  #include <phys253setup.txt>
+  Serial.begin(9600);
+}
+
+void loop()
+{   
+  int leftErr = analogRead(LEFT_IR);
+  int rightErr = analogRead(RIGHT_IR);
+  
+  int totalErr = (leftErr - rightErr) / (leftErr + rightErr); //normalize
+
+  if (totalErr != prevErr) {
+    errTime = 0;
+    prevErrTime = errTime; //take slope from change before previous change
+  }
+
+  errDeriv = (totalErr - prevErr) / (errTime + prevErrTime);
+  int correct = (PTape.Value * totalErr + DTape.Value * errDeriv);
+  if(correct > Speed.Value * 2) correct = Speed.Value * 2;
+  if(correct < -Speed.Value * 2) correct = -Speed.Value * 2;
+  
+  int leftMotor, rightMotor;
+  leftMotor = (correct > 0) ? Speed.Value : Speed.Value + correct;
+  rightMotor = (correct < 0) ? -Speed.Value : -Speed.Value + correct;
+  
+  motor.speed(LEFT_MOTOR, leftMotor);
+  motor.speed(RIGHT_MOTOR, rightMotor);
+  
+  if(i == 100){
+    if(startbutton()){
+      delay(50);
+      if(startbutton()){
+        motor.speed(LEFT_MOTOR, 0);
+        motor.speed (RIGHT_MOTOR, 0);
+        Menu();
+      }
+    }
+    
+    if(stopbutton()){
+      delay(50);
+      if(stopbutton()){
+        motor.speed(LEFT_MOTOR, 0);
+        motor.speed (RIGHT_MOTOR, 0);
+        delay(BUTTON_WAIT);
+        while(!stopbutton()){
+          int leftSensor = analogRead(LEFT_IR);
+          int rightSensor = analogRead(RIGHT_IR);
+          LCD.clear();
+          LCD.home();
+          LCD.print(leftSensor);
+          LCD.setCursor(0, 1);
+          LCD.print(rightSensor);
+          delay(50);
+        }  
+      }
+      delay(BUTTON_WAIT);
+    }
+    
+    LCD.clear();
+    LCD.home();
+    char buffer[1024];
+    sprintf(buffer, "%d %d %d", leftErr, rightErr, PTape.Value);
+    LCD.print(buffer);
+    LCD.setCursor(0, 1);
+    sprintf(buffer, "%d %d %d", correct, leftMotor, rightMotor);
+    LCD.print(buffer);
+    i = 0;
+  }
+  
+  prevErr = totalErr;
+  errTime = errTime + 1;
+  i = i + 1;
+}

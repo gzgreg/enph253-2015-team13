bfb60c576854bec2b5217c286f18d1e37c6e3b0b
CaptNapalm <james.wasteneys@gmail.com>
2015-07-06 22:44:34 -0700

    Finished arm mount

---
+++ Lab/Riley's Lab Stimulations V2/Untitled3.m
@@ -0,0 +1,9 @@
+load('June3TransientHeating-Test2-ALLSYSTEMSNOMINAL.mat');
+
+load('June17BlackRodHorizontal-Heating.mat');
+
+load('June10VertBareRodHeating-BottomPowered.mat');
+
+load('June10VertBareRodHeating-TopPower.mat');
+
+load('June12MoistRodHeating.mat');

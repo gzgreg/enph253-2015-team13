85ae2e5546dd6e090373e7e39d5749b6f6161efd
CaptNapalm <james.wasteneys@gmail.com>
2015-07-10 16:46:03 -0700

    Arm edits

--- Robot/Code/ServoTest/servoTest.ino
+++ Robot/Code/ServoTest/servoTest.ino
@@ -10,11 +10,15 @@ void setup()
 }
 
 void loop(){
-  if(startbutton()){
+  RCServo1.write(knob(6)*180.0/1023.0);
+  LCD.home();LCD.print(knob(6)*180.0/1023.0);
+  
+/* if(startbutton()){
     delay(50);
     if(startbutton()){
       if(pos) RCServo1.write(180); else RCServo1.write(0);
       pos = !pos;
     }
   }
+  */
 }

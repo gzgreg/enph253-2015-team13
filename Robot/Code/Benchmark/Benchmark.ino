2a86abf811ae434cf6f1dfdf2383fdc01c6f4909
CaptNapalm <james.wasteneys@gmail.com>
2015-07-28 18:02:02 -0700

    fuck

--- Robot/Code/Benchmark/Benchmark.ino
+++ Robot/Code/Benchmark/Benchmark.ino
@@ -12,33 +12,33 @@ void setup()
 {
   #include <phys253setup.txt>
   Serial.begin(9600);
-  
-  i = 0;
-  ti = micros();
-  while(i < RUNS){
-    i++;
-  }
-  tf = micros();
-  dt = tf - ti;
-  
-  i = 0;
-  ti = micros();
-  while(i < RUNS){
-    sin(20);
-    i++;
-  }
-  tf = micros();
-  dtFunc = tf - ti;
-  
-  LCD.clear();
-  LCD.home();
-  LCD.print(dt);
-  LCD.setCursor(0, 1);
-  LCD.print(dtFunc);
-  
+//  
+//  i = 0;
+//  ti = micros();
+//  while(i < RUNS){
+//    i++;
+//  }
+//  tf = micros();
+//  dt = tf - ti;
+//  
+//  i = 0;
+//  ti = micros();
+//  while(i < RUNS){
+//    sin(20);
+//    i++;
+//  }
+//  tf = micros();
+//  dtFunc = tf - ti;
+//  
+//  LCD.clear();
+//  LCD.home();
+//  LCD.print(dt);
+//  LCD.setCursor(0, 1);
+//  LCD.print(dtFunc);
+//  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  motor.speed(0, knob(6));
 }

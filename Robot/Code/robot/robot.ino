a8d9dc837c870f70531fc4757c7c6cf6089f2fa5
CaptNapalm <james.wasteneys@gmail.com>
2015-07-29 15:00:08 -0700

    fddf

--- Robot/Code/robot/robot.ino
+++ Robot/Code/robot/robot.ino
@@ -1,99 +1,114 @@
-#include "preprocessor.h"
+#include "constants.h"
+#include "arm.h"
+#include "tape.h"
+#include "ir.h"
+#include "interrupts.h"
 #include <phys253.h>
 #include <LiquidCrystal.h>
 
-int errDeriv, prevErr, errTime, prevErrTime, threshold, i;
+int mode;
 
 void setup()
 {
   #include <phys253setup.txt>
   Serial.begin(9600);
+  mode = 0;
+  petNum = 1;
+  leftRotations = 0;
+  rightRotations = 0;
+  onMarking = false;
+  
+  enableExternalInterrupt(INT0, RISING);
+  enableExternalInterrupt(INT1, RISING);
+  //enableExternalInterrupt(INT2, FALLING);
+  
+  ARM_RELEASE.write(180);
+  int newMode;
+  while(!startbutton()){
+    LCD.clear(); LCD.home();
+    newMode = map(knob(6), 0, 1024, 1, 8);
+    LCD.print(newMode);
+    delay(50);
+  }
+  mode = newMode;
+  //moveArm(78, 900, 0); //initial arm position
+  state = TAPE_FOLLOW;
 }
-
+int j = 0; //temp
 void loop()
-{   
-  int leftErr = (analogRead(LEFT_SENSOR) < Thresh.Value) ? 1 : 0;
-  int rightErr = (analogRead(RIGHT_SENSOR) < Thresh.Value) ? 1 : 0;
-  int leftMark = (analogRead(L_MARK_SENSOR) < Thresh.Value) ? 1 : 0;
-  int rightMark = (analogRead(R_MARK_SENSOR) < Thresh.Value) ? 1 : 0;
-
-  int totalErr = leftErr - rightErr;
-  
-  if (leftErr == 1 && rightErr == 1) {
-    //off line: set error to be same sign as previous error
-    if (prevErr < 0) totalErr = -3;
-    if (prevErr >= 0) totalErr = 3;
-  }
-  
-  if(leftMark == 1){
-    if(rightMark == 1 || leftErr == 1 || rightErr == 1){
-      //on tape marking: do pet pickup
-    } else totalErr = 10;
-  } else if(rightMark == 1){
-    if(leftErr == 1 || rightErr == 1){
-      //on tape marking: do pet pickup
-    } else totalErr = -10;
-  }
-
-  if (totalErr != prevErr) {
-    errTime = 0;
-    prevErrTime = errTime; //take slope from change before previous change
-  }
-
-  errDeriv = (totalErr - prevErr) / (errTime + prevErrTime);
-  int correct = (PTape.Value * totalErr - DTape.Value * errDeriv);
-  if(correct > Speed.Value * 2) correct = Speed.Value * 2;
-  if(correct < -Speed.Value * 2) correct = -Speed.Value * 2;
-  
-  int leftMotor, rightMotor;
-  leftMotor = (correct > 0) ? Speed.Value : Speed.Value + correct;
-  rightMotor = (correct < 0) ? -Speed.Value : -Speed.Value + correct;
-  
-  motor.speed(LEFT_MOTOR, leftMotor);
-  motor.speed(RIGHT_MOTOR, rightMotor);
-  
-  if(i == 100){
-    if(startbutton()){
-      delay(50);
-      if(startbutton()){
-        motor.speed(LEFT_MOTOR, 0);
-        motor.speed (RIGHT_MOTOR, 0);
-        Menu();
-      }
+{
+  if(mode == 1){ //marking detection
+    switch(state){
+      case TAPE_FOLLOW:
+        tapeFollow();
+        j=0;
+        break;
+      case PET_PICKUP:
+        motor.stop_all();
+        delay(1000);
+        int currRotLeft = leftRotations;
+        int currRotRight = rightRotations;
+        while(leftRotations - currRotLeft < 4 && rightRotations - currRotRight < 4 && j == 0){
+          if(leftRotations - currRotLeft < 4) motor.speed(LEFT_MOTOR, -150);
+          if(rightRotations - currRotRight < 4) motor.speed(RIGHT_MOTOR, 150);
+          LCD.print(leftRotations - currRotLeft);
+        }
+        motor.stop_all();
+        LCD.clear(); LCD.home();
+        LCD.print(leftRotations);
+        LCD.setCursor(0, 1);
+        LCD.print(rightRotations);
+        j++;
+        
+        break;
     }
-    
-    if(stopbutton()){
-      delay(50);
-      if(stopbutton()){
-        motor.speed(LEFT_MOTOR, 0);
-        motor.speed (RIGHT_MOTOR, 0);
-        delay(BUTTON_WAIT);
-        while(!stopbutton()){
-          int leftSensor = analogRead(LEFT_SENSOR);
-          int rightSensor = analogRead(RIGHT_SENSOR);
-          LCD.clear();
-          LCD.home();
-          LCD.print(leftSensor);
-          LCD.setCursor(0, 1);
-          LCD.print(rightSensor);
-          delay(50);
-        }  
-      }
-      delay(BUTTON_WAIT);
-    }
-    
-    LCD.clear();
-    LCD.home();
+  } else if(mode == 2){ //arm movement
+    moveArm(-1, knob(6), map(knob(7), 0, 1024, 0, 180));
+    LCD.clear(); LCD.home();
     char buffer[1024];
-    sprintf(buffer, "%d %d %d", Speed.Value, PTape.Value, DTape.Value);
+    sprintf(buffer, "%d %d %d", baseAngle, joint1Angle, joint2Angle);
+    LCD.print(buffer);    
+  } else if(mode == 3){ //IR follow
+    state = IR_FOLLOW;
+    irFollow();
+  } else if(mode == 4){
+    state = PET_PICKUP;
+    petPickup(petNum);
+    delay(1000);
+  } else if(mode == 5){
+    LCD.clear(); LCD.home(); LCD.print(digitalRead(ARM_END));
+    delay(50);
+  } else if(mode == 6){
+    motor.speed(LEFT_MOTOR, 150);
+    motor.speed(RIGHT_MOTOR, 150);
+    LCD.clear(); LCD.home(); LCD.print(leftRotations); LCD.setCursor(0, 1); LCD.print(rightRotations);
+  } else if(mode == 7){
+    LCD.clear(); LCD.home();
+    char buffer[1024];
+    sprintf(buffer, "%d %d %d %d", analogRead(L_MARK_SENSOR), analogRead(LEFT_SENSOR), analogRead(RIGHT_SENSOR), analogRead(R_MARK_SENSOR));
     LCD.print(buffer);
-    LCD.setCursor(0, 1);
-    sprintf(buffer, "%d %d %d", correct, leftMotor, rightMotor);
-    LCD.print(buffer);
-    i = 0;
+    delay(50);
+  }
+  if(stopbutton()){
+    delay(50);
+    if(stopbutton()){
+      int newMode;
+      while(!startbutton()){
+        LCD.clear(); LCD.home();
+        newMode = map(knob(6), 0, 1024, 1, 8);
+        LCD.print(newMode);
+        delay(50);
+      }
+      mode = newMode;
+      if(mode == 1) state = TAPE_FOLLOW;
+    }
   }
   
-  prevErr = totalErr;
-  errTime = errTime + 1;
-  i = i + 1;
+  if(startbutton()){
+    delay(50);
+    if(startbutton()){
+      motor.stop_all();
+      Menu();
+    }
+  }
 }
